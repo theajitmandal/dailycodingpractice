bbcc6b9dfae0f5f86814dd0ee848c0ee6e52bee6
Ajit Mandal <theajitmandal@gmail.com>
2024-11-28 19:37:53 +0545

    just practice

---
+++ js/one.js
@@ -0,0 +1,107 @@
+// 21st Nov, 2024 (6th Mangsir 2081)
+
+/*
+    JavaScript (JS) is the most popular lightweight, scripting, and interpreted programming language. 
+    It was developed by Brendan Eich in 1995. JavaScript is well-known as a scripting language for web pages, 
+    mobile apps, web servers, and many other platforms. It is essential for both front-end and back-end developers 
+    to have a strong command of JavaScript, as many job roles require fluency in this language.
+*/
+
+// JS Interview Questions (From Geeks For Geeks)
+
+/*
+
+    1. What are the differences between Java and JavaScript?
+
+        Java is an object Oriented Programming language while 
+        JavaScript is a client-side scripting language. Both of them are totally different from each other.
+
+        Java: It is one of the most popular programming languages. 
+        It is an object-oriented programming language and has a virtual machine platform that allows you to 
+        create compiled programs that run on nearly every platform. Java promised, “Write Once, Run Anywhere”.
+
+        JavaScript: It is a light-weighted programming language (“scripting language”) for developing interactive 
+        web pages. It can insert dynamic text into the HTML elements. JavaScript is also known as the browser’s language.
+
+    // 25th Nov, 2024 (10th Mangsir, 2081)
+
+    2. What are Data Types in JavaScript?
+
+        JavaScript data types are categorized into two parts i.e. primitive and non-primitive types.
+
+        Primitive Data Type: The predefined data types provided by JavaScript language are known as primitive data type. 
+            Primitive data types are also known as in-built data types.
+            Numbers
+            Strings
+            Boolean
+            Symbol
+            Undefined
+            Null
+            BigInt
+
+        Non-Primitive Data Type: The data types that are derived from primitive data types are known as non-primitive data types. 
+        It is also known as derived data types or reference data types.
+            Objects
+            Functions
+            Arrays
+
+    3. Which symbol is used for comments in JavaScript? 
+    
+        Comments prevent the execution of statements. Comments are ignored while the compiler executes the code. 
+        
+        There are two type of symbols to represent comments in JavaScript:
+
+            a. Double slash: It is known as a single-line comment. 
+
+                            // Single Line Comment
+
+            b. Slash with Asterisk: It is known as a multi-line comment.
+
+                            /* 
+                            Multi-line comments
+                            ...
+                            */
+
+/*
+    27th Nov, 2024 (12th Mangsir, 2081)
+
+    4. What would be the result of 3+2+”7″?
+        Here, 3 and 2 behave like an integer, and “7” behaves like a string. So 3 plus 2 will be 5. 
+        Then the output will be 5+”7″ = 57.
+
+    5. What is the use of the isNaN function?
+        The number isNan function determines whether the passed value is NaN (Not a number) and is of the type “Number”. 
+        In JavaScript, the value NaN is considered a type of number. It returns true if the argument is not a number, 
+        else it returns false.
+
+    6. Which is faster in JavaScript and ASP script?
+        JavaScript is faster compared to ASP Script. JavaScript is a client-side scripting language and does not 
+        depend on the server to execute. 
+        The ASP script is a server-side scripting language always dependable on the server.
+
+    28th Nov, 2024 (12th Mangsir, 2081)
+
+    7. What is negative infinity?
+        The negative infinity is a constant value represents the lowest available value. 
+        It means that no other number is lesser than this value. 
+        It can be generate using a self-made function or by an arithmetic operation. JavaScript shows 
+        the NEGATIVE_INFINITY value as -Infinity.
+
+    8. Is it possible to break JavaScript Code into several lines?
+        Yes, it is possible to break the JavaScript code into several lines in a string statement. 
+        It can be broken by using the backslash n ‘\n’. 
+
+        For example:
+            console.log("A Online Computer Science Portal\n for Geeks")
+
+        The code-breaking line is avoid by JavaScript which is not preferable.
+            let gfg= 10, GFG = 5,
+            Geeks =
+            gfg + GFG;
+
+    9. Which company developed JavaScript?
+        Netscape developed JavaScript and was created by Brenden Eich in the year of 1995.
+
+
+
+*/
